3486e889cb9fae6b04badb27371bd912e24210bc
jiahan <Jiahan_@outlook.com>
2023-01-02 23:58:44 +0800

    添加项目文件。

---
+++ StructExtens.cs
@@ -0,0 +1,13 @@
+﻿namespace JiaHan.Tools;
+public static class StructExtens
+{
+    public unsafe static ref T ToStruct<T>(this byte[] bytes, long offset = 0) where T : unmanaged
+    {
+        fixed (void* ptr = &bytes[offset]) return ref *(T*)ptr;
+    }
+
+    public unsafe static Span<T> ToStructs<T>(this byte[] bytes, int count, long offset = 0) where T : unmanaged
+    {
+        fixed (void* ptr = &bytes[offset]) return new Span<T>(ptr, count);
+    }
+}
